5459cc23ff720db00243d28bdcd40823812aedc6
22basada <22basada@ths.school.nz>
2024-08-12 15:13:01 +1200

    august 12 2024
    added stopwatch in corner of the screen to show the progress of the game

--- scripts/game.gd
+++ scripts/game.gd
@@ -3,7 +3,8 @@ extends Node2D
 @onready var pause_menu = $"CanvasLayer/Pause Menu"
 var paused = false 
 
-var TimeBetweenRounds = 5
+#var TimeBetweenRounds = 5
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("pausemenu"):
@@ -19,18 +20,18 @@ func pauseMenu():
 		Engine.time_scale = 0
 	
 
-func _ready():
-	Game()
-
-func Game():
-	print("Round 1 startin now")
-	#Run 1st round
-	await get_tree().create_timer(TimeBetweenRounds).timeout
-	print("Round 2 startin now")
-	#Run 2nd round
-	await get_tree().create_timer(TimeBetweenRounds).timeout
-	print("Round 3 startin now")
-	#Run 3rd round
-	await get_tree().create_timer(10).timeout
-	print("Round 4 startin now")
-	#Run 4th round
+#func _ready():
+#	Game()
+#
+#func Game():
+#	print("Round 1 startin now")
+#	#Run 1st round
+#	await get_tree().create_timer(TimeBetweenRounds).timeout
+#	print("Round 2 startin now")
+#	#Run 2nd round
+#	await get_tree().create_timer(TimeBetweenRounds).timeout
+#	print("Round 3 startin now")
+#	#Run 3rd round
+#	await get_tree().create_timer(10).timeout
+#	print("Round 4 startin now")
+#	#Run 4th round
