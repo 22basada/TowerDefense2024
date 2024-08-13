0c3022c04400ef98c25d37cb7c1266f9489770ab
22basada <22basada@ths.school.nz>
2024-08-13 22:28:54 +1200

    august 13 2024
    added so when mouse is pressed it goes down and when it is released it goes up

--- scripts/game.gd
+++ scripts/game.gd
@@ -2,17 +2,28 @@ extends Node2D
 
 @onready var pause_menu = $"CanvasLayer/Pause Menu"
 var paused = false 
-var openmouse = load("res://Sprites/kenney_cursor-pack/Vector/Basic/hand_open.svg")
-var closedmouse = load("res://Sprites/kenney_cursor-pack/Vector/Basic/hand_closed.svg")
+var openmouse : Texture2D = load("res://Sprites/kenney_cursor-pack/Vector/Basic/hand_open.svg")
+var closedmouse : Texture2D = load("res://Sprites/kenney_cursor-pack/Vector/Basic/hand_closed.svg")
+var openthemouse : bool = true
+var mousedown : bool = false 
+func _ready():
+	Input.set_custom_mouse_cursor(openmouse)
 #var TimeBetweenRounds = 5
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if InputEventMouseButton:
-		Input.set_custom_mouse_cursor(closedmouse)
+	if Input.is_action_pressed("leftclick") != mousedown:
+		if (Input.is_action_pressed("leftclick")):
+			Input.set_custom_mouse_cursor(closedmouse)
+			openthemouse = false
+		else:
+			Input.set_custom_mouse_cursor(openmouse)
+			openthemouse = true
+			
+		mousedown = Input.is_action_pressed("leftclick")
+		
 #	if Input.is_action_just_pressed("pausemenu"):
 #		pauseMenu()
-	
 func pauseMenu():
 	paused = !paused
 	if paused:
