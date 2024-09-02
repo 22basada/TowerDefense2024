d38012493198bb645e2dcd872a751b134401f09a
22basada <22basada@ths.school.nz>
2024-09-02 23:00:35 +1200

    september 2 2024
    school computer wasnt logged into github so did it at home
    added health ui that starts at 50 and goes down by 1, also made it so each enemy gets faster every couple seconds for difficulty

---
+++ scripts/HealthCounter.gd
@@ -0,0 +1,13 @@
+extends Label
+
+var player_health = 50
+var health = "HEALTH: "
+func _ready():
+	health_display_update()
+	
+func health_display_update() -> void:
+	set_text(health+str(player_health))
+
+func _on_enemy_reached_end() -> void:
+	player_health -= 1
+	health_display_update()
--- scripts/stone.gd
+++ scripts/stone.gd
@@ -1,18 +1,27 @@
 extends CharacterBody2D
 
 @export var speed = 150
-
+@export var speed_increase = 10 #speed increase change over that time
+@export var speed_change_interval = 3 #speed increase over time
 var health = 2 #change this for changing enemy health
-var playerhealth = 100
-var time: float = 0.0 #this was going to be for changing speed of enemies
-var seconds: int = 0
+var health_ui : Label
+var elapsed_time = 0.0 #measured amount of time between speed changes
+func _ready() -> void:
+	health_ui = get_node("/root/game/HealthUI/HealthCounter")
 
 func _process(delta):
+	elapsed_time += delta
+
+	if elapsed_time >= speed_change_interval:
+		speed += speed_increase
+		elapsed_time = 0  # reset elapsed time
+		print("Speed increased to: ", speed)
 	get_parent().set_progress(get_parent().get_progress() +speed*delta)
-	if get_parent().get_progress_ratio() == 1:
+	if get_parent().get_progress_ratio() >= 1:
 		queue_free()
+		if health_ui:
+			health_ui._on_enemy_reached_end()
 		print("enemy reached end")
-		playerhealth -=1
 		
 	if health <= 0:
 		get_parent().get_parent().queue_free() #gets rid of enemy when health is zero
